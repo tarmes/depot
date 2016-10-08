7cf737be835cc2cccaa7c728e0fdb32e9386c4b7
Trevor Armes <traarmes@gmail.com>
2016-10-07 23:51:15 -0400

    Working

---
+++ db/seeds.rb
@@ -0,0 +1,23 @@
+Product.delete_all
+Product.create(title: 'CoffeeScript',
+  description:
+    %{CoffeeScript is JavaScript done right. It provides all of JavaScript's
+	functionality wrapped in a cleaner, more succinct syntax. In the first
+	book on this exciting new language, CoffeeScript guru Trevor Burnham
+	shows you how to hold onto all the power and flexibility of JavaScript
+	while writing clearer, cleaner, and safer code.},
+  image_url:   'https://i.ytimg.com/vi/Ofmwag9eJbg/maxresdefault.jpg',
+  price: 36.00)
+# . . .
+Product.create(title: 'Tan, Small Brunette',
+  description:
+    %{Found in the surrounding beauty of Morocco, this woman is not only willing, but eager to please you.},
+  image_url: 'http://www.short-haircut.com/wp-content/uploads/2016/11/Brunette-Bobs.jpg',
+  price: 69.69)
+# . . .
+
+Product.create(title: 'Big Tittied Blonde',
+  description:
+    %{Wet and ready. No further description required.},
+  image_url: 'http://data.whicdn.com/images/51623072/large.jpg',
+  price: 10.00)
