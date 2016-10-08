b1fe854017a2cc0b14ccebd3386b10af1cd1f029
Trevor Armes <traarmes@gmail.com>
2016-10-08 00:11:08 -0400

    Working

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,14 +7,15 @@ Product.create(title: 'CoffeeScript',
 	shows you how to hold onto all the power and flexibility of JavaScript
 	while writing clearer, cleaner, and safer code.},
   image_url:   'https://i.ytimg.com/vi/Ofmwag9eJbg/maxresdefault.jpg',
+  image_url:   'home/work/depot/pic/coffee.jpg',
   price: 36.00)
-# . . .
+
 Product.create(title: 'Tan, Small Brunette',
   description:
     %{Found in the surrounding beauty of Morocco, this woman is not only willing, but eager to please you.},
   image_url: 'http://www.short-haircut.com/wp-content/uploads/2016/11/Brunette-Bobs.jpg',
+  image_url: 'home/work/depot/pic/ec.jpg',
   price: 69.69)
-# . . .
 
 Product.create(title: 'Big Tittied Blonde',
   description:
